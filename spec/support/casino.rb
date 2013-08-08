3de1e73cd90d35c6a2acfa57cbe5b6bbd1e9a5d2
Derek Lindahl <dlindahl@customink.com>
2013-08-07 21:15:29 -0400

    Uses ActiveSupport::Configurable to reimplements CASinoCore::Settings
    This handles auto-loading the cas.yml file as well as adding the
    CASino-specific front-end settings.

--- spec/support/casino.rb
+++ spec/support/casino.rb
@@ -1,6 +1,17 @@
+require 'active_support/core_ext/hash/deep_dup'
+
 RSpec.configure do |config|
   config.around(type: :controller) do
     self.routes = CASino::Engine.routes
+  end
+
+  config.before do
+    @base_config = CASino.config.deep_dup
+  end
+
+  config.after do
+    CASino.config.clear
+    CASino.config.merge! @base_config
   end
 end
 

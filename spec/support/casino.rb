7ae8cdfbb033b05959b1506b396ac775fcd3ff40
Derek Lindahl <dlindahl@customink.com>
2013-08-07 21:15:13 -0400

    [temporary] Temporarily sets an explicit table names in CASinoCore
    Due to the namespacing changes required for developing a standard
    Rails engine, the default table names inferred in CASinoCore were
    wrong. This temporarily works around that by setting explicit
    values until the CASinoCore::Model namespaced classes are absorbed
    into this project.

--- spec/support/casino.rb
+++ spec/support/casino.rb
@@ -3,3 +3,23 @@ RSpec.configure do |config|
     self.routes = CASino::Engine.routes
   end
 end
+
+class CASinoCore::Model::LoginTicket < ActiveRecord::Base
+  self.table_name = 'casino_login_tickets'
+end
+
+class CASinoCore::Model::ServiceTicket < ActiveRecord::Base
+  self.table_name = 'casino_service_tickets'
+end
+
+class CASinoCore::Model::TicketGrantingTicket < ActiveRecord::Base
+  self.table_name = 'casino_ticket_granting_tickets'
+end
+
+class CASinoCore::Model::TwoFactorAuthenticator < ActiveRecord::Base
+  self.table_name = 'casino_two_factor_authenticators'
+end
+
+class CASinoCore::Model::User < ActiveRecord::Base
+  self.table_name = 'casino_users'
+end

5e34b8922b81fe5cb995bab5b9698c10ff688914
Derek Lindahl <dlindahl@customink.com>
2013-08-07 21:29:21 -0400

    Adds ProxyGrantingTickets

---
+++ app/models/casino/proxy_granting_ticket.rb
@@ -0,0 +1,7 @@
+class CASino::ProxyGrantingTicket < ActiveRecord::Base
+  attr_accessible :iou, :ticket, :pgt_url
+  validates :ticket, uniqueness: true
+  validates :iou, uniqueness: true
+  belongs_to :granter, polymorphic: true
+  has_many :proxy_tickets, dependent: :destroy
+end

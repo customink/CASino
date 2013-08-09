be1663fb182007b7e01108d653d3440e99e88491
Derek Lindahl <dlindahl@customink.com>
2013-08-09 01:04:56 -0400

    Removes CASinoCore references from ServiceTicketsController specs

--- spec/controllers/service_tickets_controller_spec.rb
+++ spec/controllers/service_tickets_controller_spec.rb
@@ -4,7 +4,7 @@ describe CASino::ServiceTicketsController do
   describe 'GET "validate"' do
     let(:params) { { service: 'https://www.example.com/' } }
     it 'calls the process method of the LegacyValidator' do
-      CASinoCore::Processor::LegacyValidator.any_instance.should_receive(:process).with(kind_of(Hash)) do |params|
+      CASino::LegacyValidator.any_instance.should_receive(:process).with(kind_of(Hash)) do |params|
         params.should == controller.params
         controller.render nothing: true
       end
@@ -15,7 +15,7 @@ describe CASino::ServiceTicketsController do
   describe 'GET "serviceValidate"' do
     let(:params) { { service: 'https://www.example.com/' } }
     it 'calls the process method of the LegacyValidator' do
-      CASinoCore::Processor::ServiceTicketValidator.any_instance.should_receive(:process).with(kind_of(Hash)) do |params|
+      CASino::ServiceTicketValidator.any_instance.should_receive(:process).with(kind_of(Hash)) do |params|
         params.should == controller.params
         controller.render nothing: true
       end

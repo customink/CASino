a8f7f4443a1f6817c6747332760b0e65af237f85
Derek Lindahl <dlindahl@customink.com>
2013-08-09 11:25:09 -0400

    Formally installs CASino into the dummy test application

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,10 +11,10 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130724180136) do
+ActiveRecord::Schema.define(:version => 20130809161242) do
 
   create_table "casino_login_tickets", :force => true do |t|
-    t.string   "ticket"
+    t.string   "ticket",     :null => false
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
